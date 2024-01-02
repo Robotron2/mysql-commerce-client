fd750b278ceebc699563da23f080aca2f125556d
Theophilus Adeleke <theophilusadesola002@gmail.com>
2024-01-02 13:17:45 +0100

    Fix: Random products by random-category bugs in state and logic fixed.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react-hooks/exhaustive-deps */
+import { useEffect, useState } from "react"
+import axios from "axios"
 import { Link, useNavigate } from "react-router-dom"
+
+import ProductList from "../Products/components/ProductList"
 import Header from "../../Components/Layouts/Header"
 import Footer from "../../Components/Layouts/Footer"
 import { motion } from "framer-motion"
-// import LazyLoad from "../../../../src/assets/lazy.png"
-import LazyLoad from "../../../src/assets/lazy.png"
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react"
@@ -15,55 +17,28 @@ import "swiper/swiper-bundle.css"
 // Import Swiper styles
 import "swiper/css"
 import CategoryLinks from "./components/CategoryLinks"
-
-import SubNav from "./componennts/SubNav"
-import { useEffect, useState } from "react"
-import axios from "axios"
-import ProductList from "../Products/components/ProductList"
-
-import toast from "react-hot-toast"
-import UseProduct from "../Admin/hooks/UseProduct"
+import { LazyLoadImage } from "react-lazy-load-image-component"
 
 function Home() {
-	const { randomcategoryId } = UseProduct()
-	const [products, setProducts] = useState([])
-	const [categoryData, setCategoryData] = useState({})
-	const [page, setPage] = useState(1)
 	const [showCaseImages, setShowCaseImages] = useState([])
-	const [categoryId, setCategoryId] = useState(null)
+	const [randomCategories, setRandomCategories] = useState([])
+	const [randomCategoryData, setRandomCategoryData] = useState({ name: null, id: null })
 	const navigate = useNavigate()
 
 	const baseUrl = import.meta.env.VITE_REACT_APP_API
-	const sliderArray = ["Image", "Image2", "Image", "Image2"]
-	const categoryArray = [1, 2, 3, 4]
 
 	const showCaseProducts = async () => {
 		const response = await axios.get(`${baseUrl}/products/get-featured`)
 		if (response.data?.success) {
 			setShowCaseImages(response.data?.featuredProduct)
-		}
-
-		// setCategoryId(categories[randomCategoryId])
-	}
-	console.log(randomcategoryId)
-
-	const randomProducts = async () => {
-		try {
-			const response = await axios.get(`${baseUrl}/products/category/get-product?categoryId=${randomcategoryId}`)
-			console.log(response)
-
-			if (response.data.success) {
-				setProducts(response.data?.randomCategoryProducts)
-			}
-		} catch (error) {
-			console.log(error)
-			toast.error("Something went wrong")
+			setRandomCategories(response.data?.randomCategoryProducts)
+			setRandomCategoryData(response.data?.randomCategoryData)
 		}
 	}
+
 	useEffect(() => {
 		showCaseProducts()
-		categoryId !== 0 && randomProducts()
-	}, [page])
+	}, [])
 
 	return (
 		<motion.div animate={{ opacity: 1 }} initial={{ opacity: 0 }} exit={{ opacity: 0 }} transition={{ duration: 0.8 }}>
@@ -96,13 +71,10 @@ function Home() {
 					>
 						{showCaseImages.length > 0 &&
 							showCaseImages.map((item, i) => {
-								// let imagePath = item.Image?.filePath.replace("/public//g", "")
-
 								return (
 									<SwiperSlide key={i}>
 										<Link to={`/category/${item.category}`}>
 											<img src={item?.image} alt="product-img" className="w-full object-center h-[420px] rounded-md" />
-											{/* <img src={`${baseUrl}/${imagePath}`} alt="product-img" className="w-full object-center h-[420px] rounded-md" /> */}
 										</Link>
 									</SwiperSlide>
 								)
@@ -112,8 +84,6 @@ function Home() {
 				<div className="show-grid col-span-4 lg:col-span-1 grid grid-cols-4 gap-4 shadow-xl">
 					{showCaseImages.length > 0 &&
 						showCaseImages.map((item, i) => {
-							// let imagePath = item.Image?.filePath.replace("/public//g", "")
-
 							return (
 								<div className=" col-span-2 rounded" key={i}>
 									<Link to={`/category/${item.category}`}>
@@ -123,48 +93,70 @@ function Home() {
 								</div>
 							)
 						})}
-					{/* <div className=" col-span-2 rounded">
-						<Link>
-							<img src={Image2} alt="image" className=" object-cover h-full rounded-md" />
-						</Link>
-					</div>
-					<div className=" col-span-2 rounded">
-						<Link>
-							<img src={Image2} alt="image" className=" object-cover h-full rounded-md" />
-						</Link>
-					</div>
-					<div className=" col-span-2">
-						<Link>
-							<img src={Image2} alt="image rounded" className=" object-cover h-full rounded-md" />
-						</Link>
-					</div>
-					<div className=" col-span-2 rounded">
-						<Link>
-							<img src={Image2} alt="image" className=" object-cover h-full rounded-md" />
-						</Link>
-					</div> */}
 				</div>
 			</div>
 
-			<div className="top-category bg-inherit px-6 sm:px-8 md:px-14 lg:px-20 mt-6">
-				<div className=" bg-gray-900/90 rounded-tr-lg rounded-tl-lg flex justify-between p-3 text-white font-bold shadow-xl">
-					<h4 className="text-2xl">{categoryData?.name}</h4>
-					<Link to={`/category/${categoryData?.id}`}>
-						<h6 className="font-semibold cursor-pointer">See more</h6>
-					</Link>
-				</div>
-				<ProductList products={products} />
-			</div>
+			{randomCategoryData !== null && (
+				<div className="top-category bg-inherit px-6 sm:px-8 md:px-14 lg:px-20 mt-6">
+					<div className=" bg-gray-900/90 rounded-tr-lg rounded-tl-lg flex justify-between p-3 text-white font-bold shadow-xl">
+						<h4 className="text-2xl capitalize">{randomCategoryData?.name}</h4>
+						<Link to={`/category/${randomCategoryData?.id}`}>
+							<h6 className="font-semibold cursor-pointer">See more</h6>
+						</Link>
+					</div>
+					{/* <ProductList products={randomCategories} /> */}
+					<div className="grid grid-cols-12 gap-4 pt-8">
+						{randomCategories.map((product) => {
+							return (
+								<Link
+									to={`/products/${product.id}`}
+									key={product.id}
+									className="col-span-12 md:col-span-6 lg:col-span-3 bg-white text-gray-800 h-full mx-2 rounded-md shadow-lg "
+								>
+									<div>
+										<div className="bg-white rounded-md transform lg:hover:scale-105 transition duration-300">
+											<div className="h-60 w-full overflow-hidden">
+												<LazyLoadImage
+													alt="Product Image"
+													src={product.image}
+													className=" object-cover rounded-md w-full h-full "
+													placeholderSrc="../../../../src/assets/lazy.png"
+												/>
+											</div>
 
-			{/* <div className="top-category bg-inherit px-6 sm:px-8 md:px-14 lg:px-20 mt-6">
-				<div className=" bg-gray-900/90 rounded-tr-lg rounded-tl-lg flex justify-between p-3 text-white font-bold shadow-xl">
-					<h4 className="text-2xl">Women</h4>
-					<Link to={"/products"}>
-						<h6 className="font-semibold cursor-pointer">See more</h6>
-					</Link>
+											<div className="p-2 rounded-md bg-white">
+												<h2>
+													<span className="font-semibold">Product Name: </span>
+													{product.name.substring(0, 15) + "..."}
+												</h2>
+												<h2>
+													<span className="font-semibold">Category: </span>
+													{product.name}
+												</h2>
+												<p>
+													<span className="font-semibold"> Quantity: </span>
+													{product.countInStock}
+												</p>
+												{product?.description !== undefined && (
+													<p>
+														<span className="font-semibold">Description: </span>
+														{product.description.substring(0, 15) + "..."}
+													</p>
+												)}
+
+												<p>
+													<span className="font-semibold">Price: </span>
+													<span className="font-bold ml-1">${product.price}</span>
+												</p>
+											</div>
+										</div>
+									</div>
+								</Link>
+							)
+						})}
+					</div>
 				</div>
-				<ProductList products={products} />
-			</div> */}
+			)}
 
 			{/* <Footer /> */}
 		</motion.div>
