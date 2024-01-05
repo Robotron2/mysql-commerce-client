bf1578cd16bd10e7ec2b34868ccf81fed4491957
Theophilus Adeleke <theophilusadesola002@gmail.com>
2024-01-05 14:36:37 +0100

    Progress: User cart can now be viewed on the cartpage.

--- src/Components/Layouts/CartComponent.jsx
+++ src/Components/Layouts/CartComponent.jsx
@@ -7,6 +7,7 @@ import toast from "react-hot-toast"
 
 const CartComponent = () => {
 	const { cartLength, fetchCart, setFetchCart, totalValue, cartItems } = UseCart()
+	console.log(cartItems)
 	const baseUrl = `${import.meta.env.VITE_REACT_APP_API}`
 	const handleRemoveItem = async (itemId) => {
 		const localAuth = JSON.parse(localStorage.getItem("accessToken"))
@@ -29,15 +30,22 @@ const CartComponent = () => {
 		<div>
 			<div className=" px-6 sm:px-8 md:px-14 lg:px-20 mt-6 mx-auto lg:grid lg:grid-cols-6 gap-4">
 				{cartItems.map((item, i) => {
-					let imagePath = item.Product.Image.filePath.replace("/public//g", "")
+					let imagePath = item.product.image
 					// console.log(item)
 					return (
 						<div className="item bg-gray-100 rounded-md my-2 lg:col-span-2 grid grid-cols-5  gap-2 sm:gap-5 shadow-md p-2 sm:p-4 md:p-6" key={i + 1}>
 							<div className="h-20 sm:h-28 md:h-40 w-full overflow-hidden col-span-2">
-								<LazyLoadImage alt="Product Image" src={`${baseUrl}/${imagePath}`} className=" object-cover rounded-md w-full h-full " placeholderSrc="../../../src/assets/lazy.png" />
+								<LazyLoadImage
+									alt="Product Image"
+									src={imagePath}
+									className=" object-cover rounded-md w-full h-full "
+									placeholderSrc="../../../src/assets/lazy.png"
+								/>
 							</div>
 							<div className="product-text col-span-3 grid grid-cols-1">
-								<span className="text-md break-words">{item.Product.productName.length > 15 ? item.Product.productName.substring(0, 15) + "..." : item.Product.productName}</span>
+								<span className="text-md break-words">
+									{item.product.name.length > 15 ? item.product.name.substring(0, 15) + "..." : item.product.name}
+								</span>
 								<span className="text-md">Price: ${item.price}</span>
 								<span className="text-md">Quantity: {item.quantity}</span>
 								<span>
--- src/Pages/Products/context/CartProvider.jsx
+++ src/Pages/Products/context/CartProvider.jsx
@@ -6,17 +6,16 @@ import useAuth from "../../../Components/CustomHooks/UseAuth"
 
 // eslint-disable-next-line react/prop-types
 const CartProvider = ({ children }) => {
-	const [cart, setCart] = useState(null)
+	// const [cart, setCart] = useState(null)
 	const [totalValue, setTotalValue] = useState(0)
 	const [cartLength, setCartLength] = useState(0)
 	const [cartItems, setCartItems] = useState([])
 	const [fetchCart, setFetchCart] = useState(false)
 	const [auth] = useAuth()
-	// console.log(auth)
 
-	const updateCart = (newCart) => {
-		setCart(newCart)
-	}
+	// const updateCart = (newCart) => {
+	// 	setCart(newCart)
+	// }
 
 	const updateTotalValue = (newValue) => {
 		setTotalValue(newValue)
@@ -31,46 +30,92 @@ const CartProvider = ({ children }) => {
 	const localAuth = JSON.parse(localStorage.getItem("accessToken"))
 	const apiEndpoint = `${import.meta.env.VITE_REACT_APP_API}`
 	const fetchCartData = async () => {
-		const response = await axios.get(`${apiEndpoint}/carts`, {
+		const response = await axios.get(`${apiEndpoint}/carts/cart-info`, {
 			headers: {
-				accessToken: `${localAuth?.token}`,
+				Authorization: localAuth,
 			},
 		})
 		if (response.data.success) {
-			updateCart(response.data.cart)
+			// updateCart(response.data?.userCart.cart)
+			updateTotalValue(response.data?.total)
+			updateCartLength(response.data?.length)
+			updateCartItems(response.data?.userCart.cart)
 		}
-		// console.log(response.data.cart)
+		// console.log(response.data)
 	}
 
-	const fetchCartItemsTotal = async () => {
-		const response = await axios.get(`${apiEndpoint}/carts/total`, {
-			headers: {
-				accessToken: `${localAuth?.token}`,
-			},
-		})
-		if (response.data.success) {
-			updateTotalValue(response.data.totalValue)
-			updateCartLength(response.data.cart.CartItems.length)
-			updateCartItems(response.data.cart.CartItems)
-			// console.log(response.data.cart.CartItems.length)
-			// console.log(response.data.totalValue)
-			// console.log(response.data)
-		}
-	}
+	// {
+	//     "userCart": {
+	//       "_id": "65970f8959d6339631b92ff3",
+	//       "cart": [
+	//         {
+	//           "quantity": 2,
+	//           "product": {
+	//             "_id": "659172029ddc090e1db0a339",
+	//             "name": "Product two",
+	//             "image": "http://localhost:4000/public/upload/Accessories---Bluetooth-Ultra-Thin-Wireless-Mouse-2.4G-Charging-Black.jpg-1704040345468.jpeg",
+	//             "price": 20,
+	//             "id": "659172029ddc090e1db0a339"
+	//           },
+	//           "_id": "65972b9ff05a029354ceb3bc",
+	//           "id": "65972b9ff05a029354ceb3bc"
+	//         },
+	//         {
+	//           "quantity": 1,
+	//           "product": {
+	//             "_id": "659171969ddc090e1db0a333",
+	//             "name": "Product one",
+	//             "image": "http://localhost:4000/public/upload/Accessories---Ace-Elec-F9-Wireless-Fingerprint-Touch-Bluetooth-Headset.jpg-1704030614450.jpeg",
+	//             "price": 20,
+	//             "id": "659171969ddc090e1db0a333"
+	//           },
+	//           "_id": "65972daaf05a029354ceb413",
+	//           "id": "65972daaf05a029354ceb413"
+	//         },
+	//         {
+	//           "quantity": 1,
+	//           "product": {
+	//             "_id": "65940fc53f79dea27a772523",
+	//             "name": "Apple MacBook Pro 16.2 inches - 32GB - 1TB - 10 Cores - M1 Max",
+	//             "image": "http://localhost:4000/public/upload/Computer---Apple-MacBook-Pro-16.2-inches---32GB---1TB---10-Cores---M1-Max---Silver.jpg-1704202181899.jpeg",
+	//             "price": 349.99,
+	//             "id": "65940fc53f79dea27a772523"
+	//           },
+	//           "_id": "65972db2f05a029354ceb431",
+	//           "id": "65972db2f05a029354ceb431"
+	//         },
+	//         {
+	//           "quantity": 1,
+	//           "product": {
+	//             "_id": "659411083f79dea27a7725a7",
+	//             "name": "Hp Envy 15 X360 Touchscreen-12th Gen Intel Core I7, 16GB RAM,1TB SSD",
+	//             "image": "http://localhost:4000/public/upload/Computer---Hp-Envy-15-X360-Touchscreen-12th-Gen-Intel-Core-I7,-16GB-RAM,1TB-SSD,-Backlit-Keyboard,-Wins-11.jpg-1704202504519.jpeg",
+	//             "price": 299.99,
+	//             "id": "659411083f79dea27a7725a7"
+	//           },
+	//           "_id": "65972db8f05a029354ceb43b",
+	//           "id": "65972db8f05a029354ceb43b"
+	//         }
+	//       ],
+	//       "id": "65970f8959d6339631b92ff3"
+	//     },
+	//     "total": 709.98,
+	//     "length": 4
+	// }
+
 	useEffect(() => {
 		fetchCartData()
-		fetchCartItemsTotal()
 	}, [fetchCart, auth])
 
 	return (
 		<CartContext.Provider
 			value={{
-				cart,
+				// cart,
 				totalValue,
 				cartLength,
 				fetchCart,
 				cartItems,
-				updateCart,
+				// updateCart,
 				updateCartItems,
 				setCartLength,
 				updateTotalValue,
